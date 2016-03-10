eea7122ea89d270025a8aa7efa4a7b28779071f5
Max Häkkä <hakka@kth.se>
2016-03-10 14:05:55 +0100

    Fixed conflicts

--- app/js/dishCtrl.js
+++ app/js/dishCtrl.js
@@ -1,12 +1,6 @@
 // Dinner controller that we use whenever we want to display detailed
 // information for one dish
 dinnerPlannerApp.controller('DishCtrl', function ($scope,$routeParams,Dinner) {
-<<<<<<< HEAD
-=======
-  // TODO in Lab 5: you need to get the dish according to the routing parameter
-  // $routingParams.paramName
-  // Check the app.js to figure out what is the paramName in this case
->>>>>>> 1807bbb86f1fb05039629d461ede2a8074c36319
   // TODO in Lab 5: you need to get the dish according to the routing parameter
   // $routingParams.paramName
   // Check the app.js to figure out what is the paramName in this case
